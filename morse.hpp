56499aa0cd4995491ccd1e4a4685a5747ef75ad1
em-rosePetals <105890503+em-rosePetals@users.noreply.github.com>
2022-05-24 08:37:54 -0700

    Added readBin function

--- morse.hpp
+++ morse.hpp
@@ -5,6 +5,7 @@
 void writeString(const std::string& str, const std::string& dir);
 std::vector<bool> stringToMorseBits(const std::string& str);
 
+std::vector<bool> readBin(const std::string& dir);
 std::string readString(const std::string& dir);
 std::string morseBitsToString(const std::vector<bool>& bits);
 
